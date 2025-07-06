ba8c30bba3d674a9dca8e3c2c78636df9626a340
willian017 <willianyuji100@gmail.com>
2025-07-06 00:06:41 -0300

    feat: icon file

---
+++ src/Icones.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Icones.css';
+
+
+function Icones() {
+    return (
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    );
+}
+
+export default Icones;
