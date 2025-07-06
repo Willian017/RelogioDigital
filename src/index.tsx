5b31330561b466b9f48a2948bca4c5bbc86936ad
willian017 <willianyuji100@gmail.com>
2025-07-06 00:07:02 -0300

    feat: index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Icones from './Icones';
 import reportWebVitals from './reportWebVitals';
+import Relogio from './Relogio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='index-div'>
+      <Relogio />
+      <Icones />
+    </div>
   </React.StrictMode>
 );
 
