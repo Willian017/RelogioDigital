6edb81407e7bc9eeb1e6ee8d8895a3e711788a0b
willian017 <willianyuji100@gmail.com>
2025-08-08 17:00:38 -0300

    feat: enhance stopwatch page with styling and functionality improvements

--- src/pages/stopwatch.tsx
+++ src/pages/stopwatch.tsx
@@ -1,23 +1,60 @@
-import { useEffect, useState } from "react";
-import { BiAlarm, BiBell, BiFullscreen } from "react-icons/bi"
-import { Link } from 'react-router-dom'
+import { BiAlarm, BiBell, BiCaretRight, BiFullscreen } from "react-icons/bi"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
-export function Stopwatch(){
-    const horas=0
-    const minutos=0
-    const segundos=0
+export function Stopwatch() {
+    const formatar = (valor: number) => String(valor).padStart(2, '0');
 
-    return(
+    const horas = formatar(0);
+    const minutos = formatar(0);
+    const segundos = formatar(0);
+
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    const [horaAtual, setHoraAtual] = useState<Date>(new Date());
+
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+        setHoraAtual(new Date()); 
+        }, 1000);
+
+        return () => clearInterval(intervalo); 
+    }, []);
+
+    const toggleFullScreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen().catch((err) => {
+                console.error(`Erro ao tentar entrar em tela cheia: ${err.message} (${err.name})`)
+            })
+        } else {
+            document.exitFullscreen()
+        }
+    };
+
+    useEffect(() => {
+        const onFullScreenChange = () => {
+            setIsFullScreen(!!document.fullscreenElement)
+        }
+
+        document.addEventListener('fullscreenchange', onFullScreenChange)
+
+        return () => document.removeEventListener('fullscreenchange', onFullScreenChange)
+    }, [])
+
+    return (
         <div className="flex items-center justify-center flex-col h-screen bg-black">
-            <div className="flex text-[16rem] font-bold text-white">
+            <div className="flex text-[16rem] font-bold text-white stopwatch">
                 <span className="Hora">{horas}</span>:
                 <span className="Minuto">{minutos}</span>:
-                <span className="Segundo">{segundos}</span>
+                <span className="Segundo">{segundos}</span>:
+                <span className="Segundo text-[10rem]">{segundos}</span>
             </div>
             <div className="flex">
-                <BiAlarm className="icons mr-6"/>
-                <BiBell className="icons mr-6"/>
-                <BiFullscreen onClick={toggleFullScreen} className="icons mr-6"/>
+                <BiCaretRight className="icons mr-6" />
+                <Link to={`/`}> 
+                   <BiAlarm className="icons mr-6"/> 
+                </Link>
+                <BiFullscreen onClick={toggleFullScreen} className="icons mr-6" />
             </div>
         </div>
     )
